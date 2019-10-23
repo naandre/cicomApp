73a1a0ac0d1bc751ca3950c6ab6e4dc80480e379
Jaime Norato <jn901113jn@gmail.com>
2019-10-22 22:11:26 -0500

    formuario de filtro

--- lib/screens/articles/search_trips.dart
+++ lib/screens/articles/search_trips.dart
@@ -1,10 +1,57 @@
+import 'package:cicom/screens/gradient_back.dart';
 import 'package:flutter/material.dart';
 
-class SearchTrips extends StatelessWidget{
+class SearchTrips extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _SearchTrips();
+  }
+}
+
+class _SearchData{
+   String filter1='';
+   String value1='';
+   String filter2='';
+   String value2='';
+   DateTime date;
+}
+
+class _SearchTrips extends State<SearchTrips>{
+  final GlobalKey<FormState> _fromKey= GlobalKey<FormState>();
+  _SearchData _data=_SearchData();
+
+  String _validateDate(DateTime value){
+    if(value.toString().isEmpty){
+      return "La correo Fecha de Publicación no puede estar vació";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
+    return Stack(
+      children: <Widget>[
+        GradientBack('Buscar articulo', 100),
+        Container(
+          margin: EdgeInsets.only(
+              top: 120,
+              left:20,
+              right: 20
+          ),
+          child: Form(
+            key: this._fromKey,
+            child: ListView(
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.datetime,
+                  decoration: InputDecoration(
+                    labelText: "Fecha de Publicación"
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 
