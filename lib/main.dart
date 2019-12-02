7d5ee39014373131853db1f4291a784c0e606133
Jaime Norato <jn901113jn@gmail.com>
2019-12-01 22:32:54 -0500

    implementando bloc + singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cicom/User/bloc/bloc_user.dart';
 import 'package:cicom/widgets/menu_trips_cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,12 +10,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application. #0073a1
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cicom App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      bloc: UserBloc(),
+      child: MaterialApp(
+        title: 'Cicom App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MenuTripsCupertino(),
       ),
-      home: MenuTripsCupertino(),
     );
   }
 }
