1d7063bd409acffbe93bb2b300dc7f76b9502bf2
Jaime Norato <jn901113jn@gmail.com>
2019-12-04 20:40:55 -0500

    implementando graphql

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cicom/User/bloc/bloc_user.dart';
 import 'package:cicom/widgets/menu_trips_cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application. #0073a1
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
+
+    final HttpLink httpLink=HttpLink(uri: "http://cicom.miscursosweb.com.co/graphql");
+    ValueNotifier<GraphQLClient> client=ValueNotifier(GraphQLClient(
+      cache: InMemoryCache(),
+      link: httpLink,
+    ));
+
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Cicom App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MenuTripsCupertino(),
+      ),
+    );
+
+    /*return BlocProvider(
       bloc: UserBloc(),
       child: MaterialApp(
         title: 'Cicom App',
@@ -19,6 +39,7 @@ class MyApp extends StatelessWidget {
         ),
         home: MenuTripsCupertino(),
       ),
-    );
+    );*/
+
   }
 }
